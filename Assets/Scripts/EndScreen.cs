f2370bf9a77bd8d2aadcd5058692a0aeb878a68e
Will Reynard <44241583+wjreynard@users.noreply.github.com>
2020-08-02 17:01:41 +0100

    changed end text and added new sounds

--- Assets/Scripts/EndScreen.cs
+++ Assets/Scripts/EndScreen.cs
@@ -21,7 +21,7 @@ public class EndScreen : MonoBehaviour
         {
             holder.SetActive(true);
             animator.SetBool("Active", true);
-            finalScoreText.text = "Y0U SC0RED: " + player.score.ToString();
+            finalScoreText.text = "Y0U SC0RED " + player.score.ToString() + " / 6  C0INS";
 
             // restart on SPACE press
             if (Input.GetButtonDown("Jump"))
